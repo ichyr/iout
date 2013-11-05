18f091d8afe653da799c894bd2ae90d651ca7b64
ichyr <ichyr@softserveinc.com>
2013-11-05 16:51:29 +0200

    added initial RoR progect

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iout::Application.config.secret_token = '7c5a81f4e2cbd67921bb7722b88a22f3a1aa168425c4aeef4ba415a1c0d04e780b6af1f8903e596421e5058dd52b3d94512a73f01fa957f1d544960e58d9c0b5'
